a0889d294454fd02ed7203fabd2ce82de43d960c
Nakata Riccardo <149764397+Rikiza89@users.noreply.github.com>
2025-11-19 15:46:27 +0900

    Update urls.py

--- family_budget/urls.py
+++ family_budget/urls.py
@@ -19,11 +19,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
+# Non-translated URLs (ONLY i18n endpoint)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('budget.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
+# Translated URLs (all app URLs go here)
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('budget.urls')),
+    prefix_default_language=False
+)
+
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
