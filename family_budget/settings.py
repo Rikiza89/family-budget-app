ecfd9f220bccf7aaa5157f470cdd1a55a4795af6
Nakata Riccardo <149764397+Rikiza89@users.noreply.github.com>
2025-11-20 11:02:46 +0900

    Update settings.py

--- family_budget/settings.py
+++ family_budget/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-x)2($pgenxr(l*s3+20myd(lp5emf6b)j8=noelvwyq_0okz5x
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
